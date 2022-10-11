cb725091be5b4e9e0027b9a7de76fe27075f2427
Charles H Andrus <charles.h.andrus@gmail.com>
2022-10-11 16:56:12 +0000

    Updated changes

--- 2_call_provider_directory/__init__.py
+++ 2_call_provider_directory/__init__.py
@@ -1,12 +1,15 @@
 import requests
 
-url = "https://production.api.centene.com/fhir/providerdirectory/InsurancePlan?name=az"
+url = "https://production.api.centene.com/fhir/providerdirectory/Practitioner?family=andrus"
 
 payload={}
 headers = {
   'Content-Type': 'application/json'
 }
-
+print("Start Data Retrieval")
 response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+print("Data Retrieved")
+data = response.json()
+print("Data Parsed")
+for i in range(len(data["entry"])):
+  print(data["entry"][i]["resource"]["name"][0]["text"])
